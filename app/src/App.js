41b9cdad836f9f045c2014ce44e665c03d2714c9
Iouri Volkov <volk.iouri@gmail.com>
2022-02-21 16:04:08 -0500

    load packages, check if user has phantom wallet installed

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,46 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+import React, { useEffect } from "react";
+import "./App.css";
 
 const App = () => {
+  // Actions
+
+  // check window obj in DOM to see if Phantom extension has injected the SOL object
+  // if have SOL obj -> check to see if it's a Phantom wallet
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana && solana.isPhantom) {
+        console.log("Phantom wallet found!");
+
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log(
+          "Connected with Public key:",
+          response.publicKey.toString()
+        );
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // called once on component mount (empty dep arr) -> when user visits app check if they have Phantom wall installed
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          <p className="header">Doge Zillas</p>
+          <p className="sub-text">3 good boys borking on Solana</p>
         </div>
       </div>
     </div>
